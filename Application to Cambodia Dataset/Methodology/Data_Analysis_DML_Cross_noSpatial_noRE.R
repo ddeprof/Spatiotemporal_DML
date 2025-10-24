3c6d2bb40eae19c33d7998c8b724994f02ca676d
aarifin024 <aarifin@ncsu.edu>
2025-10-24 17:24:24 -0400

    Add files via upload

---
+++ Application to Cambodia Dataset/Methodology/Data_Analysis_DML_Cross_noSpatial_noRE.R
@@ -0,0 +1,158 @@
+# Import Cambodia_data and Cambodia_df
+set.seed(100)
+
+library(dbarts)
+library(caret)
+library(estimatr)
+
+# number of pixels
+n <- 476*580
+
+restrict_Y0 <- (
+  df$cover2k_v < 0.05 | is.na(df$con_v) | is.na(df$cover2k_v) | is.na(df$elev_v)
+  | is.na(df$pop_v) | is.na(df$PREC_v) | is.na(df$TEMP_v) | df$urban_v > 0.05
+  | is.na(df$water_v) | is.na(df$Y0_v) 
+)
+restrict_Y1 <- (
+  df$cover2k_v < 0.05 | is.na(df$con_v) | is.na(df$cover2k_v) | is.na(df$elev_v)
+  | is.na(df$pop_v) | is.na(df$PREC_v) | is.na(df$TEMP_v) | df$urban_v > 0.05
+  | is.na(df$water_v) | is.na(df$Y1_v) 
+)
+restrict_D <- (
+  df$cover2k_v < 0.05 | is.na(df$con_v) | is.na(df$cover2k_v) | is.na(df$elev_v)
+  | is.na(df$pop_v) | is.na(df$PREC_v) | is.na(df$TEMP_v) | df$urban_v > 0.05
+  | is.na(df$water_v) | (is.na(df$Y0_v) & is.na(df$Y1_v))
+)
+
+
+# f0
+x <- df[!restrict_Y0,c(4:5, 8:12)]
+y <- df[!restrict_Y0,1]
+# split data into 5 folds
+folds <- createFolds(y, k = 5)
+# initialize placeholder for predictions
+predict_Y0 <- rep(NA, sum(!restrict_Y0))
+# crossfitting Y0
+for (fold in seq_along(folds)) {
+  # this is the training data set
+  train_idx <- setdiff(seq_len(sum(!restrict_Y0)), folds[[fold]])
+  # ith fold
+  test_idx <- folds[[fold]]
+  # trains a bart model to predict Y0
+  bart_Y0 <- bart(x.train = x[train_idx, ], y.train = y[train_idx], keeptrees = TRUE)
+  # storing the predicted bart data into the placeholder vectors
+  bart_preds <- predict(bart_Y0, x[test_idx, ])
+  predict_Y0[test_idx] <- colMeans(bart_preds)
+}
+
+# f1
+x <- df[!restrict_Y1,c(4:5, 8:12)]
+y <- df[!restrict_Y1,2]
+# split data into 5 folds
+folds <- createFolds(y, k = 5)
+# initialize placeholder for predictions
+predict_Y1 <- rep(NA, sum(!restrict_Y1))
+# crossfitting Y1
+for (fold in seq_along(folds)) {
+  # this is the training data set
+  train_idx <- setdiff(seq_len(sum(!restrict_Y1)), folds[[fold]])
+  # ith fold
+  test_idx <- folds[[fold]]
+  # trains a bart model to predict Y1
+  bart_Y1 <- bart(x.train = x[train_idx, ], y.train = y[train_idx], keeptrees = TRUE)
+  # storing the predicted bart data into the placeholder vectors
+  bart_preds <- predict(bart_Y1, x[test_idx, ])
+  predict_Y1[test_idx] <- colMeans(bart_preds)
+}
+
+# g
+x <- df[!restrict_D,c(4:5, 8:12)]
+y <- df[!restrict_D,3]
+# split data into 5 folds
+folds <- createFolds(y, k = 5)
+# initialize placeholder for predictions
+predict_D <- rep(NA, sum(!restrict_D))
+# crossfitting D
+for (fold in seq_along(folds)) {
+  # this is the training data set
+  train_idx <- setdiff(seq_len(sum(!restrict_D)), folds[[fold]])
+  # ith fold
+  test_idx <- folds[[fold]]
+  # trains a bart model to predict D
+  bart_D <- bart(x.train = x[train_idx, ], y.train = y[train_idx], keeptrees = TRUE)
+  # storing the predicted bart data into the placeholder vectors
+  bart_preds <- predict(bart_D, x[test_idx, ])
+  predict_D[test_idx] <- colMeans(bart_preds)
+}
+
+# handle non NA values
+R_Y0_valid <- df$Y0_v[!restrict_Y0] - predict_Y0
+R_Y1_valid <- df$Y1_v[!restrict_Y1] - predict_Y1
+R_D_valid <- df$con_v[!restrict_D] - predict_D
+
+# put back into vectors of length n (number of pixels)
+R_Y0 <- rep(NA, length(df$Y0_v))
+R_Y0[!restrict_Y0] <- R_Y0_valid
+R_Y1 <- rep(NA, length(df$Y1_v))
+R_Y1[!restrict_Y1] <- R_Y1_valid
+R_D <- rep(NA, length(df$con_v))
+R_D[!restrict_D] <- R_D_valid
+
+R_matrix <- cbind(R_Y0, R_Y1)
+
+# vectorize R
+R <- as.vector(R_matrix)
+
+# calculate local mean
+R_D_m <- matrix(R_D, 476, 580)
+localR_D_m <- matrix(NA, 476, 580)
+for (i in 1:476) {
+  for (j in 1:580) {
+    # get surroundings (rook)
+    # consider all cases
+    if (i == 1) {
+      if (j == 1) {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i, j+1])
+      }
+      else if (j == 580) {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i, j-1])
+      }
+      else {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i, j+1], R_D_m[i, j-1])
+      }
+    }
+    else if (i == 476) {
+      if (j == 1) {
+        neighbors <- c(R_D_m[i-1, j], R_D_m[i, j+1])
+      }
+      else if (j == 580) {
+        neighbors <- c(R_D_m[i-1, j], R_D_m[i, j-1])
+      }
+      else {
+        neighbors <- c(R_D_m[i-1, j], R_D_m[i, j+1], R_D_m[i, j-1])
+      }      
+    }
+    else {
+      if (j == 1) {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i-1, j], R_D_m[i, j+1])
+      }
+      else if (j == 580) {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i-1, j], R_D_m[i, j-1])
+      }
+      else {
+        neighbors <- c(R_D_m[i+1, j], R_D_m[i-1, j], 
+                       R_D_m[i, j+1], R_D_m[i, j-1])
+      }
+    }
+    localR_D_m[i,j] <- mean(na.omit(neighbors))   
+  }
+}
+localR_D <- as.vector(localR_D_m)
+
+# regression
+t <- c(rep(0, n), rep(1, n))
+regress_DML <- lm(R ~ t + c(R_D, R_D) + c(localR_D, localR_D) + 
+                    t*c(R_D, R_D) + t*c(localR_D, localR_D))
+regress_robust_DML <- lm_robust(R ~ t + c(R_D, R_D) + c(localR_D, localR_D) + 
+                                  t*c(R_D, R_D) + t*c(localR_D, localR_D))
+
